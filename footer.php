4aa8c48af7f11b6468c85e3f21c8b5840b9c3150
WuTszHo <25041545S@common.cpce-polyu.edu.hk>
2026-04-05 23:41:25 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,6 +0,0 @@
-    <footer style="text-align: center; padding: 20px; margin-top: 40px; background: #333; color: #fff;">
-        <p>Copyright &copy; <?php echo date("Y"); ?> Paws & Polish Pet Spa</p>
-    </footer>
-
-</body>
-</html>
